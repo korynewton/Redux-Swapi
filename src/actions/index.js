8b83452e7627d3bed4aa0c61f7eb314b5045a70c
korynewton <korynewtondev@gmail.com>
2019-03-06 12:05:18 -0800

    starWarsReducer completed for now

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,6 +2,9 @@
 
 // we'll need to create 3 different action types here.
 // one for fetching, one for success and one for failure
+export const FETCHING = "FETCHING";
+export const SUCCESS = "SUCCESS";
+export const FAILURE = "FAILURE";
 
 // our action creator will be a function that returns a function
 // the url to fetch characters from is `https://swapi.co/api/people/`
--- src/reducers/starWarsReducer.js
+++ src/reducers/starWarsReducer.js
@@ -1,6 +1,8 @@
-import /* we need our action types here*/ "../actions";
+import { FETCHING, SUCCESS, FAILURE } "../actions";
 const initialState = {
-  characters: []
+  characters: [],
+  isFetching: false,
+  error: null
   // Array characters, Boolean fetching, null error.
 };
 export const charsReducer = (state = initialState, action) => {
@@ -8,6 +10,22 @@ export const charsReducer = (state = initialState, action) => {
     // Fill me in with the important reducers
     // action types should be FETCHING, SUCCESS and FAILURE
     // your switch statement should handle all of these cases.
+    case FETCHING: 
+      return {
+        ...state,
+        isFetching: true
+      }
+    case SUCCESS:
+      return {
+        ...state,
+        characters: action.payload,
+        isFetching: false
+      }
+    case FAILURE:
+      return {
+        ...state,
+        error: 'error, try using the force'
+      }
     default:
       return state;
   }
